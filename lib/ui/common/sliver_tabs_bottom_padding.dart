ec291b4b4f227bc289a635c10aedac522070a676
Pawan Nagar <akamrnagar@protonmail.com>
2025-02-18 19:30:16 +0530

    Added socials to bottom padding

--- lib/ui/common/sliver_tabs_bottom_padding.dart
+++ lib/ui/common/sliver_tabs_bottom_padding.dart
@@ -8,8 +8,13 @@
  *
  */
 
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:mindful/config/app_constants.dart';
+import 'package:mindful/core/extensions/ext_num.dart';
 import 'package:mindful/core/extensions/ext_widget.dart';
+import 'package:mindful/core/services/method_channel_service.dart';
+import 'package:mindful/ui/common/rounded_container.dart';
 import 'package:mindful/ui/common/styled_text.dart';
 
 class SliverTabsBottomPadding extends StatelessWidget {
@@ -20,12 +25,52 @@ class SliverTabsBottomPadding extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 140, bottom: 240),
+    return Padding(
+      padding: const EdgeInsets.only(top: 140, bottom: 240),
       child: Center(
-        child: StyledText(
-          "Made with ♥️ in 🇮🇳",
-          fontSize: 14,
+        child: Column(
+          children: [
+            /// Made with
+            const StyledText(
+              "Made with ♥️ in 🇮🇳",
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+
+            6.vBox,
+
+            /// Socials
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /// Github
+                RoundedContainer(
+                  padding: const EdgeInsets.all(6),
+                  child: const Icon(FluentIcons.code_circle_20_filled),
+                  onPressed: () => MethodChannelService.instance
+                      .launchUrl(AppConstants.githubUrl),
+                ),
+                4.hBox,
+
+                /// Email
+                RoundedContainer(
+                  padding: const EdgeInsets.all(6),
+                  child: const Icon(FluentIcons.mail_read_20_filled),
+                  onPressed: () => MethodChannelService.instance
+                      .launchUrl(AppConstants.supportEmailUrl),
+                ),
+                4.hBox,
+
+                /// Instagram
+                RoundedContainer(
+                  padding: const EdgeInsets.all(6),
+                  child: const Icon(FluentIcons.chat_multiple_20_filled),
+                  onPressed: () => MethodChannelService.instance
+                      .launchUrl(AppConstants.instagramUrl),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     ).sliver;
